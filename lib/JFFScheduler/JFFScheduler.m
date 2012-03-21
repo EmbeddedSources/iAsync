3e54119be8d055beaf7e6ba47e2c96722f9c529a
Vladimir Gorbenko <volodg@mail.ru>
2012-03-21 16:48:41 +0200

    fix core when cance finished block

--- lib/JFFScheduler/JFFScheduler.m
+++ lib/JFFScheduler/JFFScheduler.m
@@ -5,6 +5,7 @@
 
 @interface JFFScheduler ()
 
+//STODO move to ARC and remove inner properties
 @property ( nonatomic, retain ) NSMutableArray* cancelBlocks;
 @property ( nonatomic, unsafe_unretained ) dispatch_queue_t queue;
 
@@ -17,37 +18,37 @@
 
 -(void)dealloc
 {
-   [ self cancelAllScheduledOperations ];
+    [ self cancelAllScheduledOperations ];
 
-   dispatch_release( queue );
-   [ cancelBlocks release ];
+    dispatch_release( queue );
+    [ cancelBlocks release ];
 
-   [ super dealloc ];
+    [ super dealloc ];
 }
 
 -(id)init
 {
-   self = [ super init ];
+    self = [ super init ];
 
-   if ( self )
-   {
-      queue = dispatch_get_current_queue();
-      dispatch_retain( queue );
-      self.cancelBlocks = [ NSMutableArray array ];
-   }
+    if ( self )
+    {
+        queue = dispatch_get_current_queue();
+        dispatch_retain( queue );
+        self.cancelBlocks = [ NSMutableArray array ];
+    }
 
-   return self;
+    return self;
 }
 
 +(id)sharedScheduler
 {
-   [ NSThread assertMainThread ];
-   static id instance_ = nil;
-   if ( !instance_ )
-   {
-      instance_ = [ self new ];
-   }
-   return instance_;
+    [ NSThread assertMainThread ];
+    static id instance_ = nil;
+    if ( !instance_ )
+    {
+        instance_ = [ self new ];
+    }
+    return instance_;
 }
 
 -(JFFCancelScheduledBlock)addBlock:( JFFScheduledBlock )actionBlock_
@@ -57,7 +58,7 @@
     if ( !actionBlock_ )
         return [ [ ^(){ /* do nothing */ } copy ] autorelease ];
 
-    dispatch_source_t timer_ = dispatch_source_create( DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue );
+    __block dispatch_source_t timer_ = dispatch_source_create( DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue );
 
     int64_t delta_ = duration_ * NSEC_PER_SEC;
     dispatch_source_set_timer( timer_
@@ -67,37 +68,42 @@
 
     __block JFFScheduler* self_ = self;
 
-    JFFSimpleBlockHolder* cancel_timer_block_holder_ = [ [ JFFSimpleBlockHolder new ] autorelease ];
-    __block JFFSimpleBlockHolder* weak_cancel_timer_block_holder_ = cancel_timer_block_holder_;
-    cancel_timer_block_holder_.simpleBlock = ^void( void )
+    JFFSimpleBlockHolder* cancelTimerBlockHolder_ = [ [ JFFSimpleBlockHolder new ] autorelease ];
+    __block JFFSimpleBlockHolder* weak_cancel_timer_block_holder_ = cancelTimerBlockHolder_;
+    cancelTimerBlockHolder_.simpleBlock = ^void( void )
     {
+        if ( !timer_ )
+            return;
+
         dispatch_source_cancel( timer_ );
         dispatch_release( timer_ );
+        timer_ = NULL;
+
         [ self_.cancelBlocks removeObject: weak_cancel_timer_block_holder_.simpleBlock ];
     };
 
-    [ self.cancelBlocks addObject: cancel_timer_block_holder_.simpleBlock ];
+    [ self.cancelBlocks addObject: cancelTimerBlockHolder_.simpleBlock ];
 
     actionBlock_ = [ [ actionBlock_ copy ] autorelease ];
-    dispatch_block_t event_handler_block_ = [ [ ^void( void )
+    dispatch_block_t eventHandlerBlock_ = [ [ ^void( void )
     {
-        actionBlock_( cancel_timer_block_holder_.onceSimpleBlock );
+        actionBlock_( cancelTimerBlockHolder_.onceSimpleBlock );
     } copy ] autorelease ];
 
-    dispatch_source_set_event_handler( timer_, event_handler_block_ );
+    dispatch_source_set_event_handler( timer_, eventHandlerBlock_ );
 
     dispatch_resume( timer_ );
 
-    return cancel_timer_block_holder_.onceSimpleBlock;
+    return cancelTimerBlockHolder_.onceSimpleBlock;
 }
 
 -(void)cancelAllScheduledOperations
 {
-   NSMutableSet* cancel_blocks_ = [ self.cancelBlocks copy ];
-   self.cancelBlocks = nil;
-   for ( JFFCancelScheduledBlock cancel_ in cancel_blocks_ )
-      cancel_();
-   [ cancel_blocks_ release ];
+    NSMutableSet* cancelBlocks_ = [ self.cancelBlocks copy ];
+    self.cancelBlocks = nil;
+    for ( JFFCancelScheduledBlock cancel_ in cancelBlocks_ )
+        cancel_();
+    [ cancelBlocks_ release ];
 }
 
 @end
