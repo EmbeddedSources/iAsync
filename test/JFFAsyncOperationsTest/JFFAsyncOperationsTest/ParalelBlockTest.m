5c91dd5297e4c0621fab1e86d22e765650301c2a
volodg <volodg@mail.ru>
2012-03-21 23:58:24 +0200

    refactor paralel asyncOperation with builder

---
+++ test/JFFAsyncOperationsTest/JFFAsyncOperationsTest/ParalelBlockTest.m
@@ -0,0 +1,13 @@
+//
+//  ParalelBlockTest.m
+//  JFFAsyncOperationsTest
+//
+//  Created by Vladimir on 21.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ParalelBlockTest.h"
+
+@implementation ParalelBlockTest
+
+@end
