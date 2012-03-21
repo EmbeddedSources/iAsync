12bfdbe26ed3a65d605ed3ede3105749c22ba310
volodg <volodg@mail.ru>
2012-03-21 23:58:43 +0200

    refactor paralel asyncOperation with builder

--- test/JFFAsyncOperationsTest/JFFAsyncOperationsTest/ParalelBlockTest.m
+++ test/JFFAsyncOperationsTest/JFFAsyncOperationsTest/ParalelBlockTest.m
@@ -1,13 +1,50 @@
-//
-//  ParalelBlockTest.m
-//  JFFAsyncOperationsTest
-//
-//  Created by Vladimir on 21.03.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
 
-#import "ParalelBlockTest.h"
+#import <JFFAsyncOperations/Helpers/JFFCancelAyncOperationBlockHolder.h>
+#import <JFFAsyncOperations/Helpers/JFFDidFinishAsyncOperationBlockHolder.h>
+
+@interface ParalelBlockTest : GHAsyncTestCase
+@end
 
 @implementation ParalelBlockTest
+
+-(void)testParalelTask
+{
+    __block BOOL theSameThread_ = NO;
+
+    @autoreleasepool
+    {
+        dispatch_queue_t currentQueue_ = dispatch_get_current_queue();
+
+        JFFSyncOperationWithProgress progressLoadDataBlock_ = ^id( NSError** error_
+                                                                  , JFFAsyncOperationProgressHandler progressCallback_ )
+        {
+            progressCallback_( [ NSNull null ] );
+//            sleep( 0.1 );
+            return [ NSNull null ];
+        };
+        JFFAsyncOperation loader_ = asyncOperationWithSyncOperationWithProgressBlock( progressLoadDataBlock_ );
+
+        JFFDidFinishAsyncOperationHandler doneCallback_ = ^( id result_, NSError* error_ )
+        {
+            theSameThread_ = ( currentQueue_ == dispatch_get_current_queue() );
+
+            if ( result_ )
+            {
+                [ self notify: kGHUnitWaitStatusSuccess forSelector: _cmd ];
+            }
+            else
+            {
+                [ self notify: kGHUnitWaitStatusFailure forSelector: _cmd ];
+            }
+        };
+
+        loader_( nil, nil, doneCallback_ );
+    }
+
+    [ self prepare ];
+    [ self waitForStatus: kGHUnitWaitStatusSuccess timeout: 1. ];
+
+    GHAssertTrue( theSameThread_, @"OK" );
+}
 
 @end
