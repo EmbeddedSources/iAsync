665d0871518e6c012a875bd351160e83b85f8995
Vladimir Gorbenko <volodg@mail.ru>
2012-05-14 17:57:54 +0300

    fix thread races bug (coredump)

--- lib/JFFAsyncOperations/CachedAsyncOperations/JFFPropertyExtractor.h
+++ lib/JFFAsyncOperations/CachedAsyncOperations/JFFPropertyExtractor.h
@@ -6,16 +6,16 @@
 
 @interface JFFPropertyExtractor : NSObject
 
-@property ( nonatomic ) JFFPropertyPath* propertyPath;
-@property ( nonatomic ) NSObject* object;
+@property ( nonatomic, strong ) JFFPropertyPath* propertyPath;
+@property ( nonatomic, strong ) NSObject* object;
 
 //object related data
-@property ( nonatomic ) NSMutableArray* delegates;
+@property ( nonatomic, strong ) NSMutableArray* delegates;
 @property ( nonatomic, copy ) JFFCancelAsyncOperation cancelBlock;
 @property ( nonatomic, copy ) JFFAsyncOperation asyncLoader;
 @property ( nonatomic, copy ) JFFDidFinishAsyncOperationHandler didFinishBlock;
 
-@property ( nonatomic ) id property;
+@property ( nonatomic, strong ) id property;
 
 -(void)clearData;
 
