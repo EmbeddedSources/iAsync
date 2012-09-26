f79f5c22d687381aa49fa636402a2fc7b7558ee8
volodg <gorbenko.vova@gmail.com>
2012-09-26 14:38:51 +0300

    code style changes

--- test/JFFUtilsTest/Classes/JFFStringFormatTest.m
+++ test/JFFUtilsTest/Classes/JFFStringFormatTest.m
@@ -3,24 +3,24 @@
 
 @implementation JFFStringFormatTest
 
--(void)testNotAllParamsPresent
+- (void)testNotAllParamsPresent
 {
     NSString* format_ = @"%@%@%@";
     NSString* param1_ = @"1";
     NSString* param2_ = @"2";
-
+    
     NSString* result_ = [ NSString stringWithFormatCheckNill: format_, param1_, param2_, nil ];
     GHAssertTrue( result_ != nil, @"Should NOT be nil value" );
 }
 
--(void)testOneMoreParam
+- (void)testOneMoreParam
 {
-    NSString* format_ = @"%@%@";
-    NSString* param1_ = @"1";
-    NSString* param2_ = @"2";
-    NSString* param3_ = @"3";
-
-    NSString* result_ = [ NSString stringWithFormatCheckNill: format_, param1_, param2_, param3_, nil ];
+    NSString *format = @"%@%@";
+    NSString *param1 = @"1";
+    NSString *param2 = @"2";
+    NSString *param3 = @"3";
+    
+    NSString* result_ = [NSString stringWithFormatCheckNill:format, param1, param2, param3, nil];
     GHAssertTrue( result_ != nil, @"Should NOT be nil value" );
 }
 
