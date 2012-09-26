824315c6422bd21ff491f93615c82cf741d2793e
volodg <gorbenko.vova@gmail.com>
2012-09-26 13:48:21 +0300

    moved to TouchXMLHelper

--- lib/JFFRestKit/JFFRestKit/XML/jRestKitBinders.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <JFFAsyncOperations/JFFAsyncOperationsBlockDefinitions.h>
-
-
-JFFAsyncOperationBinder xmlDocumentWithDataAsyncBinder( void );
-JFFAsyncOperationBinder xmlDocumentWithStringAsyncBinder( void );
