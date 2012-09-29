ebd0c27850e34ca4f1eba5568d9c2ac260d0694b
volodg <gorbenko.vova@gmail.com>
2012-09-30 01:40:45 +0300

    code style changes

--- lib/JFFUtils/JFFUtilsTest/NSObjectInstancesCountTest.m
+++ lib/JFFUtils/JFFUtilsTest/NSObjectInstancesCountTest.m
@@ -22,19 +22,25 @@
 
 - (void)testObjectInstancesCount
 {
+    NSUInteger initialInsancesCount = [TestClassA instancesCount];
+    @autoreleasepool
     {
         TestClassA *a = [TestClassA new];
         STAssertTrue(1 == [TestClassA instancesCount] && a, @"We have instances of TestClassA");
     }
-    STAssertTrue(0 == [TestClassA instancesCount], @"We have no instances of TestClassA" );
+    STAssertTrue(initialInsancesCount == [TestClassA instancesCount], @"We have no instances of TestClassA" );
 }
 
 - (void)testObjectInstancesCountWithInheritance
 {
+    NSUInteger initialInsancesCountA = [TestClassA instancesCount];
+    NSUInteger initialInsancesCountB = [TestClassB instancesCount];
+    @autoreleasepool
     {
         id b = [TestClassB new];
-        STAssertTrue( 1 == [ TestClassB instancesCount ] && b, @"We have instances of TestClassB class" );
+        STAssertTrue(1 == [TestClassB instancesCount] && b, @"We have instances of TestClassB class");
         
+        @autoreleasepool
         {
             id a = [TestClassA new];
             STAssertTrue(1 == [TestClassA instancesCount], @"We have instances of TestClassA class");
@@ -45,8 +51,8 @@
         STAssertTrue(1 == [TestClassB instancesCount], @"We have instances of TestClassB class");
     }
     
-    STAssertTrue(0 == [TestClassA instancesCount], @"We have no instances of TestClassA class");
-    STAssertTrue(0 == [TestClassB instancesCount], @"We have no instances of TestClassB class");
+    STAssertTrue(initialInsancesCountA == [TestClassA instancesCount], @"We have no instances of TestClassA class");
+    STAssertTrue(initialInsancesCountB == [TestClassB instancesCount], @"We have no instances of TestClassB class");
 }
 
 @end
