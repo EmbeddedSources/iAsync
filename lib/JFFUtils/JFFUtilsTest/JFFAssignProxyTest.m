9d13eaa34259dd1bcf7a204f27d9de8c07c826fd
volodg <gorbenko.vova@gmail.com>
2012-09-30 01:47:27 +0300

    move test from GH to ST

---
+++ lib/JFFUtils/JFFUtilsTest/JFFAssignProxyTest.m
@@ -0,0 +1,45 @@
+#import "JFFAssignProxyTest.h"
+
+#import "JFFAssignProxy.h"
+
+@interface ProxyTargetTest : NSObject
+
+@end
+
+@implementation ProxyTargetTest
+
+- (NSUInteger)justReturnFiveNumber
+{
+    return 5;
+}
+
+@end
+
+@implementation JFFAssignProxyTest
+
+-(void)testAssignProxyDealloc
+{
+    JFFAssignProxy *proxy;
+    __block BOOL targetDeallocated = NO;
+    
+    {
+        ProxyTargetTest *target = [ProxyTargetTest new];
+        [target addOnDeallocBlock: ^void( void ) {
+            targetDeallocated = YES;
+        } ];
+        
+        proxy = [[JFFAssignProxy alloc] initWithTarget:target];
+    }
+    
+    STAssertTrue(targetDeallocated, @"Target should be dealloced");
+}
+
+-(void)testAssignProxyMethodCalls
+{
+    ProxyTargetTest *target_ = [ProxyTargetTest new];
+    
+    id proxy_ = [[JFFAssignProxy alloc] initWithTarget:target_ ];
+    STAssertTrue( 5 == [ proxy_ justReturnFiveNumber ], @"Target should be dealloced" );
+}
+
+@end
