d3b21da1b516d6a691be3ed845b7e5c126c25869
volodg <gorbenko.vova@gmail.com>
2012-09-30 01:53:13 +0300

    code style changes

--- lib/JFFScheduler/NSObject+Scheduler.m
+++ lib/JFFScheduler/NSObject+Scheduler.m
@@ -17,32 +17,30 @@
              scheduler:( JFFScheduler* )scheduler_
 {
     NSParameterAssert( scheduler_ );
-
+    
     //use signature's number params
     NSString* selectorString_ = NSStringFromSelector( selector_ );
     NSUInteger numOfArgs_ = [ selectorString_ numberOfCharacterFromString: @":" ];
     NSAssert1( numOfArgs_ == 0 || numOfArgs_ == 1
               , @"selector \"%@\" should has 0 or 1 parameters"
               , selectorString_ );
-
+    
     __unsafe_unretained id self_ = self;
-
-    JFFScheduledBlock block_ = ^void( JFFCancelScheduledBlock cancel_ )
-    {
-        if ( !repeats_ )
-        {
+    
+    JFFScheduledBlock block_ = ^void( JFFCancelScheduledBlock cancel_ ) {
+        if ( !repeats_ ) {
             [ self_ removeOnDeallocBlock: cancel_ ];
             cancel_();
         }
-
+        
         numOfArgs_ == 1
             ? objc_msgSend( self_, selector_, userInfo_ )
             : objc_msgSend( self_, selector_ );
     };
-
+    
     JFFCancelScheduledBlock cancel_ = [ scheduler_ addBlock: block_
                                                    duration: timeInterval_ ];
-    [ self addOnDeallocBlock: cancel_ ];
+    [self addOnDeallocBlock: cancel_];
 }
 
 -(void)performSelector:( SEL )selector_
