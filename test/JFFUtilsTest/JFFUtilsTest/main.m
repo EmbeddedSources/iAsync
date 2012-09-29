c43cdd81accae5d4f04623be6c145aa6788582fc
volodg <gorbenko.vova@gmail.com>
2012-09-30 01:58:32 +0300

    code style changes

--- test/JFFUtilsTest/JFFUtilsTest/main.m
+++ test/JFFUtilsTest/JFFUtilsTest/main.m
@@ -70,20 +70,16 @@ int main(int argc, char *argv[])
     setenv( "GHUNIT_AUTOEXIT" , "YES", 1 );
     NSSetUncaughtExceptionHandler(&exceptionHandler);
 
-    @autoreleasepool
-    {
+    @autoreleasepool {
         // Register any special test case classes
         //[[GHTesting sharedInstance] registerClassName:@"GHSpecialTestCase"];  
    
         int retVal = 0;
         // If GHUNIT_CLI is set we are using the command line interface and run the tests
         // Otherwise load the GUI app
-        if (getenv("GHUNIT_CLI"))
-        {
+        if (getenv("GHUNIT_CLI")) {
             retVal = [GHTestRunner run];
-        }
-        else 
-        {
+        } else  {
             retVal = UIApplicationMain(argc, argv, nil, @"GHUnitIPhoneAppDelegate");
         }
 
