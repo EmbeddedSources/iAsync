affba69b61e4aa08f78d9cdb081b3d7dc9f725aa
volodg <gorbenko.vova@gmail.com>
2012-09-30 01:47:16 +0300

    move test from GH to ST

---
+++ lib/JFFUtils/JFFUtilsTest/JFFMulticastDelegateTest.h
@@ -0,0 +1,7 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+#import <Foundation/Foundation.h>
+
+@interface JFFMulticastDelegateTest : SenTestCase
+
+@end
---
+++ lib/JFFUtils/JFFUtilsTest/JFFMulticastDelegateTest.m
@@ -0,0 +1,85 @@
+#import "JFFMulticastDelegateTest.h"
+
+@protocol TestMulticastDelegateInterface < NSObject >
+
+@required
+-(NSUInteger)justReturnFiveNumber;
+
+@end
+
+@interface TestClassForMulticast : NSObject < TestMulticastDelegateInterface >
+
+@property ( nonatomic, unsafe_unretained ) NSUInteger initialState;
+
+@end
+
+@implementation TestClassForMulticast
+
+@synthesize initialState;
+
+-(NSUInteger)justReturnFiveNumber
+{
+    return self.initialState++;
+}
+
+@end
+
+@implementation JFFMulticastDelegateTest
+
+-(void)testMutableAssignArrayAssignIssue
+{
+    __block BOOL delegateDeallocated = NO;
+    
+    JFFMulticastDelegate< TestMulticastDelegateInterface > *multicast =
+    (JFFMulticastDelegate< TestMulticastDelegateInterface >*)[JFFMulticastDelegate new];
+    
+    @autoreleasepool {
+        TestClassForMulticast *delegate = [TestClassForMulticast new];
+        NSUInteger initState_ = rand();
+        delegate.initialState = initState_;
+        
+        [delegate addOnDeallocBlock:^void(void) {
+            delegateDeallocated = YES;
+        } ];
+        
+        [multicast addDelegate:delegate];
+        
+        STAssertTrue( initState_ == [ multicast justReturnFiveNumber ], @"Contains 1 object" );
+    }
+    
+    STAssertTrue( delegateDeallocated, @"Target should be dealloced" );
+    STAssertTrue( 0 == [ multicast justReturnFiveNumber ], @"Empty array" );
+}
+
+-(void)testMulticastDelegateFirstRelease
+{
+    __block BOOL multicast_deallocated_ = NO;
+    {
+        JFFMulticastDelegate< TestMulticastDelegateInterface > *multicast =
+        (JFFMulticastDelegate< TestMulticastDelegateInterface >*)[ JFFMulticastDelegate new ];
+        
+        [multicast addOnDeallocBlock:^void(void) {
+            multicast_deallocated_ = YES;
+        } ];
+        
+        NSObject *delegate = [NSObject new];
+        [multicast addDelegate:delegate];
+    }
+    
+    STAssertTrue( multicast_deallocated_, @"Target should be dealloced" );
+}
+
+-(void)testAddDelegateTwice
+{
+    JFFMulticastDelegate< TestMulticastDelegateInterface > *multicast =
+    (JFFMulticastDelegate< TestMulticastDelegateInterface >*)[JFFMulticastDelegate new];
+    
+    TestClassForMulticast *delegate = [TestClassForMulticast new];
+    delegate.initialState = 5;
+    
+    [multicast addDelegate:delegate];
+    
+    STAssertTrue(5 == [multicast justReturnFiveNumber], @"Contains 1 object");
+}
+
+@end
