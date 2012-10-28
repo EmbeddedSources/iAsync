b0ab8499cc4569bf02d6a8d7180ac06463ff2961
volodg <gorbenko.vova@gmail.com>
2012-10-28 13:24:10 +0200

    code style changes

--- lib/JFFContacts/JFFContacts/Details/NSArray+ContactsDataFilters.m
+++ lib/JFFContacts/JFFContacts/Details/NSArray+ContactsDataFilters.m
@@ -2,34 +2,30 @@
 
 @implementation NSArray (ContactsDataFilters)
 
--(id)jffContactsSelectNotEmptyStrings
+- (id)jffContactsSelectNotEmptyStrings
 {
-    NSArray* result_ = [ self map: ^id( NSString* str_ )
-    {
-        return [ str_ stringByTrimmingWhitespaces ];
-    } ];
-
-    result_ = [ result_ select: ^BOOL( NSString* str_ )
-    {
-        return [ str_ length ] != 0;
-    } ];
-
-    return result_;
+    NSArray *result = [self map:^id(NSString *str) {
+        return [str stringByTrimmingWhitespaces];
+    }];
+    
+    result = [result select:^BOOL(NSString *str) {
+        return [str length ] != 0;
+    }];
+    
+    return result;
 }
 
--(id)jffContactsSelectWithEmailOnly
+- (id)jffContactsSelectWithEmailOnly
 {
-    NSArray* result_ = [ self map: ^id( NSString* str_ )
-    {
-        return [ str_ stringByTrimmingWhitespaces ];
+    NSArray *result = [self map:^id(NSString *str) {
+        return [str stringByTrimmingWhitespaces];
     } ];
-
-    result_ = [ result_ select: ^BOOL( NSString* str_ )
-    {
-        return [ str_ isEmailValid ];
-    } ];
-
-    return result_;
+    
+    result = [result select:^BOOL(NSString *str) {
+        return [str isEmailValid];
+    }];
+    
+    return result;
 }
 
 @end
