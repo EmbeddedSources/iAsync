3c61639e6fc63d696e0501b56f31b214ba4950a0
volodg <volodg@mail.ru>
2013-03-01 13:27:18 +0200

    merge dodikk changes

--- lib/JFFTestTools/JFFTestTools/GHAsyncTestCase+MainThreadTests.m
+++ lib/JFFTestTools/JFFTestTools/GHAsyncTestCase+MainThreadTests.m
@@ -9,28 +9,48 @@
 - (void)performAsyncRequestOnMainThreadWithBlock:(TestAsyncRequestBlock)block
                                         selector:(SEL)selector
 {
-    block = [block copy];
-    void (^autoreleaseBlock)() = ^void() {
-        @autoreleasepool {
-            void (^didFinishCallback)(void) = ^void() {
-                objc_msgSend(self,
-                             @selector(notify:forSelector:),
-                             kGHUnitWaitStatusSuccess,
-                             selector);
+    [ self performAsyncRequestOnMainThreadWithBlock: block_
+                                           selector: selector_
+                                            timeout: 30000. ];
+}
+
+-(void)waitForeverForAsyncRequestOnMainThreadWithBlock:( void (^)(JFFSimpleBlock) )block_
+                                              selector:( SEL )selector_
+{
+    [ self performAsyncRequestOnMainThreadWithBlock: block_
+                                           selector: selector_
+                                            timeout: INFINITY ];
+}
+
+-(void)performAsyncRequestOnMainThreadWithBlock:( void (^)(JFFSimpleBlock) )block_
+                                       selector:( SEL )selector_
+                                        timeout:( NSTimeInterval )timeout_;
+{
+    block_ = [ block_ copy ];
+    void (^autoreleaseBlock_)() = ^void()
+    {
+        @autoreleasepool
+        {
+            void (^didFinishCallback_)(void) = ^void()
+            {
+                objc_msgSend( self
+                             , @selector( notify:forSelector: )
+                             , kGHUnitWaitStatusSuccess
+                             , selector_ );
             };
             
-            block([didFinishCallback copy]);
+            block_( [ didFinishCallback_ copy ] );
         }
     };
     
-    objc_msgSend(self, @selector(prepare), nil);
+    objc_msgSend( self, @selector( prepare ), nil );
     
-    dispatch_async(dispatch_get_main_queue(), autoreleaseBlock);
+    dispatch_async( dispatch_get_main_queue(), autoreleaseBlock_ );
     
-    objc_msgSend(self,
-                 @selector(waitForStatus:timeout:),
-                 kGHUnitWaitStatusSuccess,
-                 30000.);
+    objc_msgSend( self
+                 , @selector( waitForStatus:timeout: )
+                 , kGHUnitWaitStatusSuccess
+                 , timeout_ );
 }
 
 + (void)load
--- lib/JFFTestTools/JFFTestTools/JFFTestTools.h
+++ lib/JFFTestTools/JFFTestTools/JFFTestTools.h
@@ -3,5 +3,6 @@
 
 #import <JFFTestTools/performTimeCalculator.h>
 #import <JFFTestTools/NSObject+InstancesCount.h>
+#import <JFFTestTools/GHAsyncTestCase+MainThreadTests.h>
 
 #endif //JFF_TEST_TOOLS_HEADER_INCLUDED
