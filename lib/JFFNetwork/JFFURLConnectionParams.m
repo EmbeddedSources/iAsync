785b5f6feb2ded3141a7f0cf7fcf4806aea9a960
dodikk <dodikk88.reg@gmail.com>
2013-04-09 15:41:40 +0300

    Fixed JFFUrlRequest copy constructor

--- lib/JFFNetwork/JFFURLConnectionParams.m
+++ lib/JFFNetwork/JFFURLConnectionParams.m
@@ -7,12 +7,18 @@
     JFFURLConnectionParams *copy = [[[self class] allocWithZone:zone] init];
     
     if (copy) {
-        copy->_url                 = [_url      copyWithZone:zone];
-        copy->_httpBody            = [_httpBody copyWithZone:zone];
-        copy->_headers             = [_headers  copyWithZone:zone];
+        copy->_url                 = [ self->_url      copyWithZone: zone];
+        copy->_httpBody            = [ self->_httpBody copyWithZone: zone];
+        copy->_httpMethod          = [ self->_httpMethod copyWithZone: zone ];
+        copy->_headers             = [ self->_headers  copyWithZone: zone];
+
+        // TODO : Make a deep copy of the stream
+        // @adk : Plain assignment pay lead to inproper resource management and inconsistency
         copy->_httpBodyStream      = _httpBodyStream;
+        
+        
         copy->_useLiveConnection   = _useLiveConnection;
-        copy->_certificateCallback = _certificateCallback;
+        copy->_certificateCallback = [ _certificateCallback copy ];
         
         //cookie storage is common object for different connections
         //do not copy it
