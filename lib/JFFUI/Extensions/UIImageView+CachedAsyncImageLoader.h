77951adf3a16107267124ef35b1befd19c15ba8b
volodg <volodg@mail.ru>
2013-04-11 19:00:35 +0300

    fix dependencies

--- lib/JFFUI/Extensions/UIImageView+CachedAsyncImageLoader.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#include <objc/runtime.h>
-
-@interface UIImageView (CachedAsyncImageLoader)
-
-- (void)setImageWithURL:(NSURL *)url andPlaceholder:(UIImage *)placeholder;
-
-@end
