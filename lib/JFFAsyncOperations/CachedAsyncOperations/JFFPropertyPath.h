85d46d1169aef534ef9f8ee17686fc57078dd2d8
volodg <volodg@mail.ru>
2013-04-27 17:29:53 +0300

    fix fill contact via properties bug

--- lib/JFFAsyncOperations/CachedAsyncOperations/JFFPropertyPath.h
+++ lib/JFFAsyncOperations/CachedAsyncOperations/JFFPropertyPath.h
@@ -2,10 +2,10 @@
 
 @interface JFFPropertyPath : NSObject
 
-@property ( nonatomic, readonly ) NSString* name;
-@property ( nonatomic, readonly ) id< NSCopying, NSObject > key;
+@property (nonatomic, readonly) NSString *name;
+@property (nonatomic, readonly) id<NSCopying, NSObject> key;
 
--(id)initWithName:( NSString* )name_
-              key:( id< NSCopying, NSObject > )key_;
+- (id)initWithName:(NSString *)name
+               key:(id< NSCopying, NSObject >)key;
 
 @end
