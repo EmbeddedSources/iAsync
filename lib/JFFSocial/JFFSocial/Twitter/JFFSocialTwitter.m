9735ba8a1a6b034f75818d85f67f60658b8ea766
volodg <volodg@mail.ru>
2013-06-18 14:32:48 +0300

    cache twitter response to avoid 18 code error

--- lib/JFFSocial/JFFSocial/Twitter/JFFSocialTwitter.m
+++ lib/JFFSocial/JFFSocial/Twitter/JFFSocialTwitter.m
@@ -9,12 +9,71 @@
 
 #import "JFFTwitterDirectMessageAlreadySentError.h"
 
+#import <JFFRestKit/JFFRestKit.h>
+#import <JFFRestKit/Details/JFFResponseDataWithUpdateData.h>
+
 #import <Twitter/Twitter.h>
 #import <Accounts/Accounts.h>
 
 #define DEFAULT_SEARCH_RADIUS 100.0f
 
 static JFFSocialTwitterDidLoginCallback globalDidLoginCallback;
+
+@interface JFFTwitterReponsesCache : NSObject <JFFRestKitCache>
+@end
+
+@implementation JFFTwitterReponsesCache
+{
+    NSCache *_cache;
+}
+
+- (NSCache *)cache
+{
+    if (!_cache) {
+        
+        _cache = [NSCache new];
+    }
+    
+    return _cache;
+}
+
+- (JFFAsyncOperation)loaderToSetData:(NSData *)data forKey:(NSString *)key
+{
+    return ^JFFCancelAsyncOperation(JFFAsyncOperationProgressHandler progressCallback,
+                                    JFFCancelAsyncOperationHandler cancelCallback,
+                                    JFFDidFinishAsyncOperationHandler doneCallback) {
+        
+        JFFResponseDataWithUpdateData *cachedData = [JFFResponseDataWithUpdateData new];
+        
+        cachedData.data       = data;
+        cachedData.updateDate = [NSDate new];
+        
+        [self.cache setObject:cachedData forKey:key];
+        
+        if (doneCallback)
+            doneCallback([NSNull new], nil);
+        
+        return JFFStubCancelAsyncOperationBlock;
+    };
+}
+
+- (JFFAsyncOperation)cachedDataLoaderForKey:(NSString *)key
+{
+    return ^JFFCancelAsyncOperation(JFFAsyncOperationProgressHandler progressCallback,
+                                    JFFCancelAsyncOperationHandler cancelCallback,
+                                    JFFDidFinishAsyncOperationHandler doneCallback) {
+        
+        if (doneCallback) {
+            
+            JFFResponseDataWithUpdateData *cachedData = [self.cache objectForKey:key];
+            doneCallback(cachedData, cachedData?nil:[JFFSilentError newErrorWithDescription:@"no data for key"]);
+        }
+        
+        return JFFStubCancelAsyncOperationBlock;
+    };
+}
+
+@end
 
 static JFFSyncOperation twitterAccountsGetter()
 {
@@ -106,53 +165,135 @@ static JFFAsyncOperation twitterAccountsLoader()
     return twitterAccountsLoader();
 }
 
++ (JFFAsyncOperationBinder)dataLoaderForIdentifier
+{
+    return ^JFFAsyncOperation(id<NSCopying> loadDataIdentifier) {
+    
+        NSString *urlString           = loadDataIdentifier[@"urlString"];
+        NSDictionary *parameters      = loadDataIdentifier[@"parameters"];
+        TWRequestMethod requestMethod = [loadDataIdentifier[@"requestMethod"] integerValue];
+        
+        JFFAsyncOperationBinder requestBinder = ^JFFAsyncOperation(NSArray *accountStroreAndAccounts) {
+            
+            TWRequest *request = [[TWRequest alloc] initWithURL:[urlString toURL]
+                                                     parameters:parameters
+                                                  requestMethod:requestMethod];
+            
+            request.account = accountStroreAndAccounts[1][0];
+            
+            JFFAsyncOperation requestOperation = jffTwitterRequest(request);
+            
+            return requestOperation;
+        };
+        
+        JFFAsyncOperation loaderOperation = bindSequenceOfAsyncOperations(twitterAccountsLoader(),
+                                                                          requestBinder,
+                                                                          nil);
+        
+        JFFAsyncOperation loader = bindSequenceOfAsyncOperations(loaderOperation,
+                                                                 twitterResponseToNSData(),
+                                                                 nil);
+        
+        return loader;
+    };
+}
+
++ (JFFAsyncBinderForIdentifier)analyzerForDataWithAnalizer:(JFFAsyncOperationBinder)ayncAnalyzer
+{
+    ayncAnalyzer = [ayncAnalyzer copy];
+    
+    return ^JFFAsyncOperationBinder(id<NSCopying> loadDataIdentifier) {
+        
+        return ^JFFAsyncOperation(NSData *data) {
+            
+            JFFAsyncOperation jsonBuilder = asyncOperationBinderJsonDataParser()(data);
+            
+            JFFAsyncOperationBinder analyzer = ^JFFAsyncOperation(id jsonObject) {
+                
+                return ayncAnalyzer(@[jsonObject, loadDataIdentifier]);
+            };
+            
+            return bindSequenceOfAsyncOperations(jsonBuilder, analyzer, nil);
+        };
+    };
+}
+
++ (JFFCacheKeyForIdentifier)cacheKeyForIdentifier
+{
+    return ^(id<NSCopying, NSObject> loadDataIdentifier) {
+        
+        return [loadDataIdentifier description];
+    };
+}
+
++ (id<JFFRestKitCache>)cache
+{
+    static id<JFFRestKitCache> result;
+    
+    if (!result) {
+        
+        result = [JFFTwitterReponsesCache new];
+    }
+    
+    return result;
+}
+
++ (JFFAsyncOperation)generalTwitterApiDataLoaderWithURLString:(NSString *)urlString
+                                                   parameters:(NSDictionary *)parameters
+                                                requestMethod:(TWRequestMethod)requestMethod
+                                                 ayncAnalyzer:(JFFAsyncOperationBinder)ayncAnalyzer
+                                   cacheDataLifeTimeInSeconds:(NSTimeInterval)cacheDataLifeTimeInSeconds
+{
+    ayncAnalyzer = [ayncAnalyzer copy];
+    
+    return ^JFFCancelAsyncOperation(JFFAsyncOperationProgressHandler progressCallback,
+                                    JFFCancelAsyncOperationHandler cancelCallback,
+                                    JFFDidFinishAsyncOperationHandler doneCallback) {
+        
+        id<NSCopying, NSObject> loadDataIdentifier =
+        @{
+          @"urlString"     : urlString,
+          @"parameters"    : parameters?:@{},
+          @"requestMethod" : @(requestMethod),
+          };
+        
+        JFFAsyncOperation loader;
+        
+        if (cacheDataLifeTimeInSeconds == 0.) {
+            
+            loader = bindSequenceOfAsyncOperations([self dataLoaderForIdentifier](loadDataIdentifier),
+                                                   [self analyzerForDataWithAnalizer:ayncAnalyzer](loadDataIdentifier),
+                                                   nil);
+        } else {
+            
+            JFFSmartUrlDataLoaderFields *args = [JFFSmartUrlDataLoaderFields new];
+            
+            args.loadDataIdentifier         = loadDataIdentifier;
+            args.dataLoaderForIdentifier    = [self dataLoaderForIdentifier];
+            args.analyzerForData            = [self analyzerForDataWithAnalizer:ayncAnalyzer];
+            args.cacheKeyForIdentifier      = [self cacheKeyForIdentifier];
+            args.cacheDataLifeTimeInSeconds = cacheDataLifeTimeInSeconds;
+            args.cache                      = self.cache;
+            
+            loader = jSmartDataLoaderWithCache(args);
+        }
+        
+        return loader(progressCallback,
+                      cancelCallback,
+                      doneCallback);
+    };
+}
+
 + (JFFAsyncOperation)generalTwitterApiDataLoaderWithURLString:(NSString *)urlString
                                                    parameters:(NSDictionary *)parameters
                                                 requestMethod:(TWRequestMethod)requestMethod
                                                  ayncAnalyzer:(JFFAsyncOperationBinder)ayncAnalyzer
 {
-    JFFAsyncOperationBinder requestBinder = ^JFFAsyncOperation(NSArray *accountStroreAndAccounts) {
-        
-        TWRequest *request = [[TWRequest alloc] initWithURL:[urlString toURL]
-                                                 parameters:parameters
-                                              requestMethod:requestMethod];
-        
-        request.account = accountStroreAndAccounts[1][0];
-        
-        JFFAsyncOperation requestOperation = jffTwitterRequest(request);
-        
-        return requestOperation;
-    };
-    
-    JFFAsyncOperation loaderOperation = bindSequenceOfAsyncOperations(twitterAccountsLoader(),
-                                                                      requestBinder,
-                                                                      nil);
-    
-    ayncAnalyzer = [ayncAnalyzer copy];
-    
-    JFFAsyncOperationBinder analyzer = ^JFFAsyncOperation(NSData *data) {
-        
-        JFFAsyncOperation jsonBuilder = asyncOperationBinderJsonDataParser()(data);
-        
-        JFFAsyncOperationBinder analyzer = ^JFFAsyncOperation(id jsonObject) {
-            
-            id context =
-            @{
-              @"url"           : urlString,
-              @"params"        : parameters?:@{},
-              @"requestMethod" : @(requestMethod),
-              };
-            
-            return ayncAnalyzer(@[jsonObject, context]);
-        };
-        
-        return bindSequenceOfAsyncOperations(jsonBuilder, analyzer, nil);
-    };
-    
-    return bindSequenceOfAsyncOperations(loaderOperation,
-                                         twitterResponseToNSData(),
-                                         analyzer,
-                                         nil);
+    return [self generalTwitterApiDataLoaderWithURLString:urlString
+                                               parameters:parameters
+                                            requestMethod:requestMethod
+                                             ayncAnalyzer:ayncAnalyzer
+                               cacheDataLifeTimeInSeconds:0.];
 }
 
 + (JFFAsyncOperation)usersNearbyCoordinatesLantitude:(double)lantitude longitude:(double)longitude
@@ -176,16 +317,23 @@ static JFFAsyncOperation twitterAccountsLoader()
 + (JFFAsyncOperation)followersLoader
 {
     NSString *urlString = @"https://api.twitter.com/1.1/followers/ids.json";
+    
     JFFAsyncOperation followersIds = [self generalTwitterApiDataLoaderWithURLString:urlString
                                                                          parameters:nil
                                                                       requestMethod:TWRequestMethodGET
-                                                                       ayncAnalyzer:jsonObjectToTwitterUsersIds()];
+                                                                       ayncAnalyzer:jsonObjectToTwitterUsersIds()
+                                                         cacheDataLifeTimeInSeconds:10.*60.];
     
     JFFAsyncOperationBinder usersForIds = ^JFFAsyncOperation(NSArray *ids) {
         
         if ([ids count] == 0) {
             return asyncOperationWithResult(@[]);
         }
+        
+        ids = [ids sortedArrayUsingComparator:^NSComparisonResult(NSString *obj1, NSString *obj2) {
+            
+            return [obj1 compare:obj2];
+        }];
         
         id params = @{
         @"user_id" : [ids componentsJoinedByString:@","],
@@ -194,7 +342,8 @@ static JFFAsyncOperation twitterAccountsLoader()
         JFFAsyncOperation result = [self generalTwitterApiDataLoaderWithURLString:@"https://api.twitter.com/1.1/users/lookup.json"
                                                                        parameters:params
                                                                     requestMethod:TWRequestMethodGET
-                                                                     ayncAnalyzer:asyncJSONObjectToTwitterUsers()];
+                                                                     ayncAnalyzer:asyncJSONObjectToTwitterUsers()
+                                                       cacheDataLifeTimeInSeconds:10.*60.];
         return result;
     };
 
