424f7b6050ab1613a673b0fc37095d8e2d9f0ef3
volodg <volodg@mail.ru>
2013-07-15 11:12:25 +0300

    add -[NSString md5Digest] method

---
+++ lib/JFFUtils/NSString/NSString+MD5.m
@@ -0,0 +1,21 @@
+#import "NSString+MD5.h"
+
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSString(MD5)
+
+- (instancetype)md5Digest
+{
+    const char *cStr = [self UTF8String];
+    unsigned char result[16];
+    CC_MD5(cStr, strlen(cStr), result); // This is the md5 call
+    return [[NSString alloc] initWithFormat:
+            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+            result[0], result[1], result[2], result[3],
+            result[4], result[5], result[6], result[7],
+            result[8], result[9], result[10], result[11],
+            result[12], result[13], result[14], result[15]
+            ];
+}
+
+@end
