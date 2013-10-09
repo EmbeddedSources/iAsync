8e80631abb92798e6dfd35cfb3638f9b43d91e34
dodikk <dodikk88.reg@gmail.com>
2013-10-09 18:16:49 +0300

    added boolean comparisson based on xor

--- lib/JFFUtils/NSObject/NSObject+IsEqualTwoObjects.m
+++ lib/JFFUtils/NSObject/NSObject+IsEqualTwoObjects.m
@@ -2,13 +2,53 @@
 
 @implementation NSObject (IsEqualTwoObjects)
 
-+ (BOOL)object:(NSObject *)object1
++(BOOL)object:(NSObject *)object1
      isEqualTo:(NSObject *)object2
 {
-    if ( nil == object1 && nil == object2 )
-        return YES;
+    JUncertainLogicStates pointerCheckResult = [ self quickCheckObject: object1
+                                                             isEqualTo: object2 ];
+    if ( ULMaybe != pointerCheckResult )
+    {
+        return (BOOL)pointerCheckResult;
+    }
 
     return [object1 isEqual:object2];
+}
+
++(BOOL)objcBoolean:( BOOL )first
+           xorWith:( BOOL )second
+{
+    return ![ self objcBoolean: first
+                     isEqualTo: second ];
+}
+
++(BOOL)objcBoolean:( BOOL )first
+         isEqualTo:( BOOL )second
+{
+    return ( first && second ) || ( !first && !second );
+}
+
+
++(JUncertainLogicStates)quickCheckObject:( id )first
+                               isEqualTo:( id )second
+{
+    BOOL isBothNil = ( nil == first ) && ( nil == second );
+    BOOL isAnyNil  = ( nil == first ) || ( nil == second );
+    
+    if ( isBothNil )
+    {
+        return ULTrue;
+    }
+    else if ( first == second )
+    {
+        return ULTrue;
+    }
+    else if ( isAnyNil )
+    {
+        return ULFalse;
+    }
+    
+    return ULMaybe;
 }
 
 @end
