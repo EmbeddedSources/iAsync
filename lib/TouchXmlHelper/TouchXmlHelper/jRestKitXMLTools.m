6c79117a0ec07c2162e1beb1fb6de29fc70bc84f
volodg <volodg@mail.ru>
2013-10-18 10:47:03 +0300

    SC_CodeStyle

--- lib/TouchXmlHelper/TouchXmlHelper/jRestKitXMLTools.m
+++ lib/TouchXmlHelper/TouchXmlHelper/jRestKitXMLTools.m
@@ -3,33 +3,34 @@
 #import "JFFRestKitParseEmptyXMLError.h"
 #import "JFFRestKitParseInvalidXMLError.h"
 
-
-CXMLDocument* xmlDocumentWithData( NSData* data_, NSError** outError_ )
+CXMLDocument *xmlDocumentWithData(NSData *data, NSError **outError)
 {
-    if ( [ data_ length ] == 0 )
-    {
-        [ [ JFFRestKitParseEmptyXMLError new ] setToPointer: outError_ ];
+    if ([data length] == 0) {
+        
+        if (NULL != outError) {
+            *outError = [JFFRestKitParseEmptyXMLError new];
+        }
         return nil;
     }
-
-    NSError* parseError_ = nil;
-
-    CXMLDocument* document_ = [ [ CXMLDocument alloc ] initWithData: data_
-                                                            options: 0
-                                                              error: &parseError_ ];
-
-    xmlErrorPtr xmlError_ = nil;
-    if ( document_ )
-        xmlError_ = xmlCtxtGetLastError( document_->xmlCtxt );
-
-    if ( !parseError_ && xmlError_ )
-    {
-        NSString* errorDescription_ = @( xmlError_->message );
-        parseError_ = [ JFFRestKitParseInvalidXMLError newErrorWithDescription: errorDescription_ ];
-        document_   = nil;
+    
+    NSError *parseError = nil;
+    
+    CXMLDocument *document = [[CXMLDocument alloc] initWithData:data
+                                                        options:0
+                                                          error:&parseError];
+    
+    xmlErrorPtr xmlError = nil;
+    if (document)
+        xmlError = xmlCtxtGetLastError(document->xmlCtxt);
+    
+    if (!parseError && xmlError) {
+        
+        NSString *errorDescription = @(xmlError->message);
+        parseError = [JFFRestKitParseInvalidXMLError newErrorWithDescription:errorDescription];
+        document   = nil;
     }
-
-    [ parseError_ setToPointer: outError_ ];
-
-    return document_;
+    
+    [parseError setToPointer:outError];
+    
+    return document;
 }
