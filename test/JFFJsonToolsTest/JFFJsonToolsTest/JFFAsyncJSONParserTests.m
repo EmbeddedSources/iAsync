a8a3e2835be977eb94ca2f93764236b76f09e7f6
volodg <volodg@mail.ru>
2013-10-29 14:56:00 +0200

    JsonTools tests added

---
+++ test/JFFJsonToolsTest/JFFJsonToolsTest/JFFAsyncJSONParserTests.m
@@ -0,0 +1,40 @@
+
+@interface JFFAsyncJSONParserTests : GHTestCase
+@end
+
+@implementation JFFAsyncJSONParserTests
+
+- (void)setUp
+{
+}
+
+- (void)tearDown
+{
+}
+
+- (void)testParseEmtyJson
+{
+    __block id blockResult;
+    __block NSError *blockError;
+    
+    void (^block)(JFFSimpleBlock) = ^(JFFSimpleBlock block) {
+        
+        NSData *data = [@"{}" dataUsingEncoding:NSUTF8StringEncoding];
+        JFFAsyncOperation loader = asyncOperationJsonDataParser(data);
+        
+        loader(nil, nil, ^(id result, NSError *error){
+            
+            blockError  = error;
+            blockResult = result;
+            block();
+        });
+    };
+    
+    performAsyncRequestOnMainThreadWithBlock(block, 10.);
+    
+    GHAssertNil(blockError, nil);
+    GHAssertNotNil(blockResult, nil);
+    GHAssertTrue([blockResult isKindOfClass:[NSDictionary class]], nil);
+}
+
+@end
---
+++ test/JFFJsonToolsTest/JFFJsonToolsTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JFFJsonToolsTest
+//
+//  Created by Владимир Горбенко on 29.10.13.
+//  Copyright (c) 2013 EmbeddedSources. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
