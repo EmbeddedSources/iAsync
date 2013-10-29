d0bc86c7ee3a7b91a6769012a378e83bc0307548
volodg <volodg@mail.ru>
2013-10-29 15:10:00 +0200

    refactor jTmpFileLoaderWithChunkedDataLoader function

--- lib/JFFRestKit/JFFRestKit/JFFDataLoadersTools.m
+++ lib/JFFRestKit/JFFRestKit/JFFDataLoadersTools.m
@@ -1,47 +1,97 @@
 #import "JFFDataLoadersTools.h"
 
 #import "JFFRestKitError.h"
-#import <JFFNetwork/Callbacks/JFFNetworkResponseDataCallback.h>
+
+static NSString *queueNameForFileAtPath(NSString *filePath)
+{
+    NSCParameterAssert(nil != filePath);
+    return [@"org.jRestKit.tmp-file-download/path=" stringByAppendingString:filePath];
+}
+
+static dispatch_queue_t queueForFileAtPath(NSString *filePath)
+{
+    NSString *queueName = queueNameForFileAtPath(filePath);
+    return dispatch_queue_get_or_create([queueName UTF8String], DISPATCH_QUEUE_SERIAL);
+}
+
+static void disposeQueueForFileAtPath(NSString *filePath)
+{
+    NSString *queueName = queueNameForFileAtPath(filePath);
+    dispatch_queue_release_by_label([queueName UTF8String]);
+}
 
 JFFAsyncOperation jTmpFileLoaderWithChunkedDataLoader(JFFAsyncOperation chunkedDataLoader)
 {
     chunkedDataLoader = [chunkedDataLoader copy];
+    
     return ^JFFCancelAsyncOperation(JFFAsyncOperationProgressHandler progressCallback,
                                     JFFCancelAsyncOperationHandler cancelCallback,
                                     JFFDidFinishAsyncOperationHandler doneCallback) {
         
-        __block NSString *filePath;
-        __block NSFileHandle *handle;
+        NSString *fileName = [[NSUUID new] UUIDString];
+        NSString *filePath = [NSString cachesPathByAppendingPathComponent:fileName];
+        __block NSFileHandle *handle = nil;
+        __block volatile BOOL canceled = NO;
         
-        __block void (^closeFile)() = ^{
+        void (^closeFile)(void) = ^(void) {
+            
             [handle closeFile];
             handle = nil;
         };
         
-        __block void (^closeAndRemoveFile)() = ^{
-            closeFile();
-
-            if (filePath)
-                [[NSFileManager defaultManager] removeItemAtPath:filePath
-                                                           error:nil];
+        __block void (^closeAndRemoveFile)(void) = ^(void) {
+            
+            canceled = YES;
+            
+            dispatch_queue_t fileQueue = queueForFileAtPath(fileName);
+            
+            dispatch_barrier_async(fileQueue, ^(void) {
+                
+                closeFile();
+                
+                if (filePath)
+                    [[NSFileManager defaultManager] removeItemAtPath:filePath
+                                                               error:nil];
+                
+                disposeQueueForFileAtPath(fileName);
+            });
         };
         
         progressCallback = [progressCallback copy];
-        JFFAsyncOperationProgressHandler progressWrapperCallback = ^(JFFNetworkResponseDataCallback *progressInfo) {
-            if (!handle) {
-                filePath = [[NSUUID new] UUIDString];
-                filePath = [NSString cachesPathByAppendingPathComponent:filePath];
-                [[NSFileManager defaultManager] createFileAtPath:filePath
-                                                        contents:nil
-                                                      attributes:nil];
-                handle = [NSFileHandle fileHandleForWritingAtPath:filePath];
-            }
-
-            //STODO write in separate thread only ( dispatch_io_create_with_path )
-            [handle writeData:progressInfo.dataChunk];
+        JFFAsyncOperationProgressHandler progressWrapperCallback = ^(NSData *dataChunk) {
+            
+            NSCParameterAssert([dataChunk isKindOfClass:[NSData class]]);
+            
+            dispatch_queue_t fileQueue = queueForFileAtPath(fileName);
+            
+            dispatch_barrier_async(fileQueue, ^(void) {
+                
+                NSArray *operations =
+                @[
+                  ^void(void) {
+                      
+                      if (!handle) {
+                          [[NSFileManager defaultManager] createFileAtPath:filePath
+                                                                  contents:nil
+                                                                attributes:nil];
+                          handle = [NSFileHandle fileHandleForWritingAtPath:filePath];
+                      }
+                  },
+                  ^void(void) {
+                      
+                      [handle writeData:dataChunk];
+                  },
+                  ];
+                
+                for (NSUInteger index = 0; index < [operations count] && !canceled; ++index) {
+                    
+                    JFFSimpleBlock operation = operations[index];
+                    operation();
+                };
+            });
             
             if (progressCallback)
-                progressCallback(progressInfo);
+                progressCallback(dataChunk);
         };
         
         cancelCallback = [cancelCallback copy];
@@ -52,12 +102,18 @@ JFFAsyncOperation jTmpFileLoaderWithChunkedDataLoader(JFFAsyncOperation chunkedD
                 cancelCallback(canceled);
         };
         
-        JFFDidFinishAsyncOperationHandler doneWrapperCallback = ^(id response, NSError *error ) {
-            id result = response;
-
+        JFFDidFinishAsyncOperationHandler doneWrapperCallback = ^(id response, NSError *error) {
+            
+            id result = response?filePath:nil;
+            
             if (response) {
-                result = filePath;
-                closeFile();
+                
+                dispatch_queue_t writerQueue  = queueForFileAtPath(fileName);
+                dispatch_barrier_sync(writerQueue, ^(void) {
+                    
+                    closeFile();
+                    disposeQueueForFileAtPath(fileName);
+                });
             }
             
             if (doneCallback) {
